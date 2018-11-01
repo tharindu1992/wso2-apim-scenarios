44ec85c6048e11e92560b3bcbc8f95b70f136906
asma <azinneera@gmail.com>
2018-11-01 11:11:41 +0530

    Add run-scenario.sh

---
+++ 01-apim-sample-scenarios/01.1-api-lifecycle-mgt/1.1.1-api-create-subscribe/run-scenario.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+repo='https://github.com/kavithasub/product-temp.git'
+TEST_DIR='product-temp'
+DIR=$2
+export DATA_BUCKET_LOCATION=$DIR
+FILE1=${DIR}/infrastructure.properties
+FILE2=${DIR}/testplan-props.properties
+PROP_KEY=keyFileLocation      #pem file
+PROP_HOST=WSO2PublicIP           #host IP
+PROP_PRODUCT_NAME=ProductName
+PROP_PRODUCT_VERSION=ProductVersion
+PROP_REMOTE_DIR=REMOTE_WORKSPACE_DIR_UNIX
+
+host=`grep -w "$PROP_HOST" ${FILE1} ${FILE2} | cut -d'=' -f2`
+key_pem=`grep -w "$PROP_KEY" ${FILE1} ${FILE2} | cut -d'=' -f2`
+REM_DIR=`grep -w "$PROP_REMOTE_DIR" ${FILE1} ${FILE2} | cut -d'=' -f2`
+user='centos'
+
+git clone $repo
+cd $TEST_DIR
+mvn clean install
+
+Echo "Copying surefire-reports to data bucket"
+
+cp -r modules/integration/tests-integration/tests-backend/target/surefire-reports ${DIR}
+ls ${DIR}
