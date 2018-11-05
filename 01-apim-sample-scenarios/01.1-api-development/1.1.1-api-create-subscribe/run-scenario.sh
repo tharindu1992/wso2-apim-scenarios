a6c0ea93b3cceae9845da1b6bfd62ef1b2653385
Asma Jabir <azinneera@gmail.com>
2018-11-05 13:22:47 +0530

    Update product repo

--- 01-apim-sample-scenarios/01.1-api-development/1.1.1-api-create-subscribe/run-scenario.sh
+++ 01-apim-sample-scenarios/01.1-api-development/1.1.1-api-create-subscribe/run-scenario.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-repo='https://github.com/kavithasub/product-temp.git'
-TEST_DIR='product-temp'
+repo='https://github.com/wso2/product-apim.git'
+TEST_DIR='product-apim'
 DIR=$2
 export DATA_BUCKET_LOCATION=$DIR
 
