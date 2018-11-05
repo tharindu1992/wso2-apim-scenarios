76ffd82730deb3be0e7048e91235997c805e2f75
Asma Jabir <azinneera@gmail.com>
2018-11-05 14:16:30 +0530

    Update branch to clone

--- 01-apim-sample-scenarios/01.1-api-development/1.1.1-api-create-subscribe/run-scenario.sh
+++ 01-apim-sample-scenarios/01.1-api-development/1.1.1-api-create-subscribe/run-scenario.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 repo='https://github.com/wso2/product-apim.git'
+branch='2.x'
 TEST_DIR='product-apim'
 DIR=$2
 export DATA_BUCKET_LOCATION=$DIR
 
-git clone $repo
+git clone -b $branch $repo
 cd $TEST_DIR
 mvn clean install
 
